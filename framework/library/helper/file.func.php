6b50bb5353c8bde56bc63fe4ebdf9a882fb88f0e
wx_alexyu01 <54874931+ha737alex@users.noreply.github.com>
2020-04-16 21:57:07 +0800

    Add files via upload

---
+++ framework/library/helper/file.func.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * RocketPhp3.0 - 简单高效的php框架
+ * @Copyright weixin:Alexyu01 QQ:40989411
+ * @License Please contact the author before using it.
+ * @Last-Modified 2019-12-31 14:07
+ * @Author weixin:Alexyu01 QQ:40989411@qq.com
+ */
+defined('ISROCKET') or exit('Access denied!');
+function file_down($filepath, $filename = '',$del = 0) {
+	if(!$filename) $filename = basename($filepath);
+	if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'msie ') !== false){
+		$filename = rawurlencode($filename);	
+	}
+	$filetype = strtolower(trim(substr(strrchr($filename, '.'), 1, 10)));
+	$filesize = sprintf("%u", filesize($filepath));
+	if(ob_get_length() !== false) @ob_end_clean();
+	header('Pragma: public');
+	header('Last-Modified: '.gmdate('D, d M Y H:i:s') . ' GMT');
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Cache-Control: pre-check=0, post-check=0, max-age=0');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Encoding: none');
+	header('Content-type: '.$filetype);
+	header('Content-Disposition: attachment; filename="'.$filename.'"');
+	header('Content-length: '.$filesize);
+	readfile($filepath);
+	if($del == 1){
+		@unlink($filepath);
+	}
+	exit;
+}
+
+//转换字节数为其他单位
+function sizecount($filesize) {
+	if ($filesize >= 1073741824) {
+		$filesize = round($filesize / 1073741824 * 100) / 100 .' GB';
+	} elseif ($filesize >= 1048576) {
+		$filesize = round($filesize / 1048576 * 100) / 100 .' MB';
+	} elseif($filesize >= 1024) {
+		$filesize = round($filesize / 1024 * 100) / 100 . ' KB';
+	} else {
+		$filesize = $filesize.' B';
+	}
+	return $filesize;
+}
